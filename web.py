df22f3ee61ce4159893ad9fd97bbfb219709df1a
Sean.Breach <Sean.Breach@Outlook.com>
2020-05-23 15:00:24 -0400

    fix: Removing bug and updating the greeting message to prove Rolling Update

--- web.py
+++ web.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = "Hello World.. It\'s been fun!"
+    message = "Hello Udacity.. It\'s been fun!"
     return render_template('index.html', message=message)
-BAD TEXT TO FAIL LINT
+
 app.run(host='0.0.0.0', port=80)
 
